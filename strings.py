a2c65ec6f299a133908d5215d3bceab66d69848f
Alexei Mamani <alexei@sfleet.mx>
2024-11-12 09:46:38 -0500

    Add some examples

---
+++ strings.py
@@ -0,0 +1,14 @@
+name = "Alexei"
+caracter = "c"
+
+print(type(name))
+print(type(caracter))
+
+name_2 = 'Alexei'
+print(type(name_2))
+
+name_3 = '''Alexei
+Mamani
+'''
+print(type(name_3))
+print(name_3)
---
+++ variables.py
@@ -0,0 +1,7 @@
+greeting = "Hey!"
+name = "Alexei"
+age = 30
+
+print(greeting)
+print(name)
+print(age)
