b86dcb1022aa082d27bed6ba8512796c3d9d9604
Alexei Mamani <alexei@sfleet.mx>
2024-11-14 12:44:51 -0500

    add math examples

---
+++ math.py
@@ -0,0 +1,21 @@
+a = 10
+b = 3
+
+print("Plus:", a + b)
+print("Minus:", a - b)
+print("Multiply:", a * b)
+print("Pow:", a ** b)
+print("Divide:", a / b)
+print("Int Divide:", a // b)
+
+print("mod:", a % b)
+
+a =+ 10
+a -= 10
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
