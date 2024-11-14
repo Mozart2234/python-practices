e0a23b5aec68e6a818d63e93556fa04292692f16
Alexei Mamani <alexei@sfleet.mx>
2024-11-14 12:49:22 -0500

    Add input examples

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+age = int(input("Enter your age: "))
+print("You are", age, "years old.")
+
+print("name is of type: ", type(name))
+print("age is of type: ", type(age))
+
